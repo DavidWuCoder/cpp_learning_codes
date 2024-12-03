348885d38b271f3ae9f4fc963515cc24162a8e19
Infinitexxx <468588126@qq.com>
2024-12-03 18:07:17 +0800

    List has finished

--- List/List/test.cpp
+++ List/List/test.cpp
@@ -1,101 +1,180 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <list>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <list>
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+////int main()
+////{
+////	std::list<int> lt1 = { 1, 2, 3, 4, 5 };
+////	int x;
+////	/*while (std::cin >> x)
+////	{
+////		auto pos = std::find(lt1.begin(), lt1.end(), x);
+////		if (pos != lt1.end())
+////		{
+////			lt1.splice(lt1.begin(), lt1, pos);
+////		}
+////		for (auto e : lt1)
+////		{
+////			std::cout << e << ' ';
+////		}
+////		std::cout << std::endl;
+////	}*/
+////
+////	//std::list<int> lt2 = { 1, 3, 66, 0, 1 };
+////	//lt2.sort(std::greater<int>());
+////	//for (auto e : lt2)
+////	//{
+////	//	std::cout << e << ' ';
+////	//}
+////
+////	std::vector<int> v1 = { 1, 3, 66, 0, 1 };
+////
+////	//sort(v1.begin(), v1.end());
+////	sort(v1.begin(), v1.end(), std::greater<int>());
+////
+////	for (auto e : v1)
+////	{
+////		std::cout << e << " ";
+////	}
+////	std::cout << std::endl;
+////
+////	return 0;
+////}
+//
+//#include "List.h"
+//
+//struct pos
+//{
+//	int _x, _y;
+//	pos(int x = 0, int y = 0)
+//		:_x(x)
+//		,_y(y)
+//	{}
+//};
+//
 //int main()
 //{
-//	std::list<int> lt1 = { 1, 2, 3, 4, 5 };
-//	int x;
-//	/*while (std::cin >> x)
-//	{
-//		auto pos = std::find(lt1.begin(), lt1.end(), x);
-//		if (pos != lt1.end())
-//		{
-//			lt1.splice(lt1.begin(), lt1, pos);
-//		}
-//		for (auto e : lt1)
-//		{
-//			std::cout << e << ' ';
-//		}
-//		std::cout << std::endl;
-//	}*/
+//	//wyl::list<int> lt1;
+//	//lt1.push_back(1);
+//	//lt1.push_back(1);
+//	//lt1.push_back(1);
+//	//lt1.push_back(1);
 //
-//	//std::list<int> lt2 = { 1, 3, 66, 0, 1 };
-//	//lt2.sort(std::greater<int>());
-//	//for (auto e : lt2)
+//	//wyl::list<int>::iterator it1 = lt1.begin();
+//
+//	//while (it1 != lt1.end())
+//	//{
+//	//	std::cout << *it1 << ' ';
+//	//	++it1;
+//	//}
+//	//std::cout << std::endl;
+//
+//	//for (auto& e : lt1)
 //	//{
 //	//	std::cout << e << ' ';
 //	//}
+//	//std::cout << '\n';
 //
-//	std::vector<int> v1 = { 1, 3, 66, 0, 1 };
+//	//wyl::list<pos> lt2;
+//	
+//	//lt2.push_back({ 1, 2 });
+//	//lt2.push_back({ 2, 1 });
 //
-//	//sort(v1.begin(), v1.end());
-//	sort(v1.begin(), v1.end(), std::greater<int>());
+//	//wyl::list<pos>::iterator it2 = lt2.begin();
 //
-//	for (auto e : v1)
+//	//for (auto& e : lt2)
+//	//{
+//	//	std::cout << e._x << ' ' << e._y;
+//	//}
+//	//std::cout << std::endl;
+//
+//
+//	//while (it2 != lt2.end())
+//	//{
+//	//	std::cout << it2->_x << ' ';
+//	//	it2++;
+//	//}
+//	//std::cout << std::endl;
+//
+//	wyl::list<int> lt2;
+//	lt2.push_back(1);
+//	lt2.push_back(2);
+//	lt2.push_back(3);
+//
+//	for (auto& e : lt2)
 //	{
-//		std::cout << e << " ";
+//		std::cout << e << ' ';
 //	}
 //	std::cout << std::endl;
 //
+//	lt2.push_front(3);
+//	lt2.push_front(2);
+//	lt2.push_front(1);
+//
+//	for (auto& e : lt2)
+//	{
+//		std::cout << e << ' ';
+//	}
+//	std::cout << std::endl;
 //	return 0;
 //}
 
-#include "List.h"
 
-struct pos
-{
-	int _x, _y;
-	pos(int x = 0, int y = 0)
-		:_x(x)
-		,_y(y)
-	{}
-};
+#include "List.h"
+#include <iostream>
 
 int main()
 {
-	//wyl::list<int> lt1;
-	//lt1.push_back(1);
-	//lt1.push_back(1);
-	//lt1.push_back(1);
-	//lt1.push_back(1);
+	//const wyl::list<int> lt1(10, 0);
+	//wyl::list<int>::const_iterator it = lt1.begin();
 
-	//wyl::list<int>::iterator it1 = lt1.begin();
-
-	//while (it1 != lt1.end())
+	//while (it != lt1.end())
 	//{
-	//	std::cout << *it1 << ' ';
-	//	++it1;
+	//	std::cout << *it << ' ';
+	//	it++;
 	//}
 	//std::cout << std::endl;
 
-	//for (auto& e : lt1)
-	//{
-	//	std::cout << e << ' ';
-	//}
-	//std::cout << '\n';
+	wyl::list<int> lt1;
+	lt1.push_back(1);
+	lt1.push_back(2);
+	lt1.push_back(3);
+	lt1.push_back(4);
+	lt1.push_front(0);
+	lt1.push_front(-1);
 
-	wyl::list<pos> lt2;
-	
-	lt2.push_back({ 1, 2 });
-	lt2.push_back({ 2, 1 });
-
-	wyl::list<pos>::iterator it2 = lt2.begin();
-
-	for (auto& e : lt2)
+	for (auto& x : lt1)
 	{
-		std::cout << e._x << ' ' << e._y;
+		std::cout << x << ' ';
 	}
 	std::cout << std::endl;
 
+	lt1.pop_back();
+	lt1.pop_front();
 
-	while (it2 != lt2.end())
+	for (auto& x : lt1)
 	{
-		std::cout << it2->_x << ' ';
-		it2++;
+		std::cout << x << ' ';
+	}
+	std::cout << std::endl;
+
+	wyl::list<int> lt2(lt1);
+
+	for (auto& x : lt2)
+	{
+		std::cout << x << ' ';
+	}
+	std::cout << std::endl;
+
+	wyl::list<int> lt3(10, 1);
+	lt2 = lt3;
+
+	for (auto& x : lt2)
+	{
+		std::cout << x << ' ';
 	}
 	std::cout << std::endl;
 	return 0;
