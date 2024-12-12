3d6acf66161c8d81876f97566d542f72fb791f23
Infinitexxx <468588126@qq.com>
2024-12-13 00:19:21 +0800

    test-12-12

--- test_12_11/test_12_11/SearshBinaryTree.h
+++ test_12_11/test_12_11/SearshBinaryTree.h
@@ -1,29 +1,230 @@
+//#pragma once
+//
+//template<class K>
+//class BSTNode
+//{
+//public:
+//	K _key;
+//	BSTNode<K>* _left = nullptr;
+//	BSTNode<K>* _right = nullptr;
+//
+//	BSTNode(const K& key)
+//		:_key(key)
+//	{}
+//};
+//
+//
+//template<class K>
+//class BSTree
+//{
+//	typedef BSTNode<K> Node;
+//
+//public:
+//	bool insert(const K& key)
+//	{
+//		if (_root == nullptr) {
+//			_root = new Node(key);
+//			return true;
+//		}
+//
+//		Node* parent = nullptr;
+//		Node* cur = _root;
+//
+//		while (cur)
+//		{
+//			parent = cur;
+//			if (cur->_key < key)
+//			{
+//
+//				cur = cur->_right;
+//			}
+//			else if (cur->_key > key)
+//			{
+//				cur = cur->_left;
+//			}
+//			else
+//			{
+//				return false;
+//			}
+//		}
+//		cur = new Node(key);
+//		if (parent->_key < key)
+//		{
+//			parent->_right = cur;
+//		}
+//		else
+//		{
+//			parent->_left = cur;
+//		}
+//		return false;
+//	}
+//
+//	bool find(const K& key)
+//	{
+//		Node* cur = _root;
+//		while (cur)
+//		{
+//			if (cur->_key < key)
+//			{
+//				cur = cur->_right;
+//			}
+//			else if (cur->_key > key)
+//			{
+//				cur = cur->_left;
+//			}
+//			else
+//			{
+//
+//				return true;
+//			}
+//			return false;
+//		}
+//	}
+//
+//	void Inorder()
+//	{
+//		_InOrder(_root);
+//		std::cout << std::endl;
+//	}
+//
+//	bool Erase(const K& key)
+//	{
+//		// 这里得带父节点的查找
+//		Node* parent = nullptr;
+//		Node* cur = _root;
+//
+//		while (cur)
+//		{
+//
+//			if (cur->_key < key)
+//			{
+//				parent = cur;
+//				cur = cur->_right;
+//			}
+//			else if (cur->_key > key)
+//			{
+//				parent = cur;
+//
+//				cur = cur->_left;
+//			}
+//			else
+//			{
+//				if (cur->_left == nullptr)
+//				{
+//					if (parent == nullptr)
+//					{
+//						_root = cur->_right;
+//					}
+//					else
+//					{
+//						if (parent->_left == cur)
+//						{
+//							parent->_left = cur->_right;
+//						}
+//						else
+//						{
+//							parent->_right = cur->_right;
+//						}
+//					}
+//					delete cur;
+//
+//				}
+//				else if (cur->_right == nullptr)
+//				{
+//					if (parent == nullptr)
+//					{
+//						_root = cur->_left;
+//					}
+//					else
+//					{
+//						if (parent->_left == cur)
+//						{
+//							parent->_left = cur->_left;
+//						}
+//						else
+//						{
+//							parent->_right = cur->_left;
+//						}
+//					}
+//					delete cur;
+//				}
+//				else
+//				{
+//					// 找右子树的最小节点
+//					Node* minRight = cur->_right;
+//					Node* minRightParent = cur;
+//					while (minRight->_left)
+//					{
+//						minRightParent = minRight;
+//						minRight = minRight->_left;
+//					}
+//
+//					cur->_key = minRight->_key;
+//
+//					if (minRightParent->_left == minRight)
+//					{
+//						minRightParent->_left = nullptr;
+//					}
+//					else
+//					{
+//						minRightParent->_right = minRight->_right;
+//					}
+//					delete minRight;
+//
+//				}
+//				return true;
+//			}
+//		}
+//
+//
+//		return false;
+//	}
+//
+//private:
+//	void _InOrder(Node* root)
+//	{
+//		if (root == nullptr)
+//		{
+//			return;
+//		}
+//		_InOrder(root->_left);
+//		std::cout << root->_key << ' ';
+//		_InOrder(root->_right);
+//	}
+//
+//private:
+//	Node* _root = nullptr;
+//};
+
+
 #pragma once
 
-template<class K>
+template<class K, class V>
 class BSTNode
 {
 public:
 	K _key;
-	BSTNode<K>* _left = nullptr;
-	BSTNode<K>* _right = nullptr;
+	V _value;
+	BSTNode<K, V>* _left = nullptr;
+	BSTNode<K, V>* _right = nullptr;
 
-	BSTNode(const K& key)
+	BSTNode(const K& key, const V& value)
 		:_key(key)
+		,_value(value)
 	{}
 };
 
 
-template<class K>
+template<class K, class V>
 class BSTree
 {
-	typedef BSTNode<K> Node;
+	typedef BSTNode<K, V> Node;
 
 public:
-	bool insert(const K& key)
+	bool Insert(const K& key, const V& value)
 	{
 		if (_root == nullptr) {
-			_root = new Node(key);
+			_root = new Node(key, value);
 			return true;
 		}
 
@@ -47,7 +248,7 @@ public:
 				return false;
 			}
 		}
-		cur = new Node(key);
+		cur = new Node(key, value);
 		if (parent->_key < key)
 		{
 			parent->_right = cur;
@@ -56,9 +257,10 @@ public:
 		{
 			parent->_left = cur;
 		}
+		return false;
 	}
 
-	bool find(const K& key)
+	Node* Find(const K& key)
 	{
 		Node* cur = _root;
 		while (cur)
@@ -73,20 +275,110 @@ public:
 			}
 			else
 			{
-
-				return true;
+				return cur;
 			}
-			return false;
 		}
+		return nullptr;
 	}
 
-	void Inorder()
+	void InOrder()
 	{
 		_InOrder(_root);
 		std::cout << std::endl;
 	}
 
+	bool Erase(const K& key)
+	{
+		// 这里得带父节点的查找
+		Node* parent = nullptr;
+		Node* cur = _root;
 
+		while (cur)
+		{
+
+			if (cur->_key < key)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				parent = cur;
+
+				cur = cur->_left;
+			}
+			else
+			{
+				if (cur->_left == nullptr)
+				{
+					if (parent == nullptr)
+					{
+						_root = cur->_right;
+					}
+					else
+					{
+						if (parent->_left == cur)
+						{
+							parent->_left = cur->_right;
+						}
+						else
+						{
+							parent->_right = cur->_right;
+						}
+					}
+					delete cur;
+
+				}
+				else if (cur->_right == nullptr)
+				{
+					if (parent == nullptr)
+					{
+						_root = cur->_left;
+					}
+					else
+					{
+						if (parent->_left == cur)
+						{
+							parent->_left = cur->_left;
+						}
+						else
+						{
+							parent->_right = cur->_left;
+						}
+					}
+					delete cur;
+				}
+				else
+				{
+					// 找右子树的最小节点
+					Node* minRight = cur->_right;
+					Node* minRightParent = cur;
+					while (minRight->_left)
+					{
+						minRightParent = minRight;
+						minRight = minRight->_left;
+					}
+
+					cur->_key = minRight->_key;
+
+					if (minRightParent->_left == minRight)
+					{
+						minRightParent->_left = nullptr;
+					}
+					else
+					{
+						minRightParent->_right = minRight->_right;
+					}
+					delete minRight;
+
+				}
+				return true;
+			}
+		}
+
+
+		return false;
+	}
 
 private:
 	void _InOrder(Node* root)
@@ -96,7 +388,7 @@ private:
 			return;
 		}
 		_InOrder(root->_left);
-		std::cout << root->_key << ' ';
+		std::cout << root->_key << ':' << root->_value << '\n';
 		_InOrder(root->_right);
 	}
 
--- test_12_11/test_12_11/test.cpp
+++ test_12_11/test_12_11/test.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <string>
+using namespace std;
 
 #include "SearshBinaryTree.h"
 
 int main()
 {
-	BSTree<int> t;
-	std::vector<int> a = { 8, 3, 1, 1, 10, 6, 4, 7, 14, 13 };
-
-	for (auto& x : a)
+	string arr[] = { "a", "c", "a", "c", "a", "a", "c", "a", "b", "a", "b" };
+	for (int i = 0; i < 7; i++)
 	{
-		t.insert(x);
+		std::cout << arr[i] << ' ';
 	}
-	t.Inorder();
+	BSTree<string, int> countTree;
+	for (const auto& str : arr)
+	{
+		// 先查找⽔果在不在搜索树中
+		// 1、不在，说明⽔果第⼀次出现，则插⼊<⽔果, 1>
+		// 2、在，则查找到的结点中⽔果对应的次数++
+		//BSTreeNode<string, int>* ret = countTree.Find(str);
+		auto ret = countTree.Find(str);
+		if (ret == nullptr)
+		{
+			countTree.Insert(str, 1);
+		}
+		else
+		{
+			ret->_value++;
+		}
+	}
+	countTree.InOrder();
 	return 0;
 }
