c7cc1f42fbbafc602031bd4cc4656ef17939fb9f
Infinitexxx <468588126@qq.com>
2025-04-27 23:07:30 +0800

    bitset

---
+++ test_04_27/test_04_27/BitSet.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <vector>
+
+namespace wyl
+{
+	template<size_t N>
+	class bit_set
+	{
+	public:
+		bit_set()
+		{
+			_bs.resize(N / 32 + 1);
+
+		}
+
+		// x映射的位标记为1
+		void set(size_t x)
+		{
+			size_t i = x / 32;
+			size_t j = x % 32;
+			_bs[i] |= (1 << j);
+		}
+
+		// x映射的位标记成0
+		void reset(size_t x)
+		{
+			size_t i = x / 32;
+			size_t j = x % 32;
+			_bs[i] &= ~(1 << j);
+		}
+
+		// 检查x是否标记为1
+		bool test(size_t x)
+		{
+			size_t i = x / 32;
+			size_t j = x % 32;
+
+			//  1 0r 0
+			return _bs[i] & (1 << j);
+		}
+
+	private:
+		std::vector<size_t> _bs;
+	};
+
+	template<size_t N>
+	class twobitset
+	{
+	public:
+		void set(size_t x)
+		{
+			bool bit1 = _bs1.test(x);
+			bool bit2 = _bs2.test(x);
+
+			if (!bit1 && !bit2)
+			{
+				_bs2.set(x);
+			}
+			else if (!bit1 && bit2)
+			{
+				_bs1.set(x);
+				_bs2.reset(x);
+			}
+			else if (bit1 && !bit2)
+			{
+				_bs2.set(x);
+			}
+		}
+
+	private:
+		bitset<N> _bs1;
+		bitset<N> _bs2;
+	};
+}
---
+++ test_04_27/test_04_27/Test.cpp
@@ -0,0 +1,39 @@
+//#include <iostream>
+#include "BitSet.h"
+//using namespace std;
+//
+//int main()
+//{
+//	wyl::bit_set<100> bs;
+//	bs.set(32);
+//	bs.set(33);
+//
+//	bs.reset(33);
+//	bs.set(34);
+//
+//	std::cout << bs.test(31) << std::endl;
+//	std::cout << bs.test(32) << std::endl;
+//	std::cout << bs.test(33) << std::endl;
+//	std::cout << bs.test(34) << std::endl;
+//
+//	wyl::bit_set<-1> bs1;
+//	wyl::bit_set<0xfffffff> bs2;
+//	wyl::bit_set<UINT_MAX> bs3;
+//	return 0;
+//}
+
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+//int main()
+//{
+//	std::cout << sizeof(std::vector<int>);
+//	return 0;
+// }
+int main()
+{
+	std::vector<double> v{ 1, 2, 3 };
+	std::cout << v.size() << std::endl;         // 输出3（元素数量）
+	std::cout << sizeof(v);        // 仍为24（对象自身大小）
+}
