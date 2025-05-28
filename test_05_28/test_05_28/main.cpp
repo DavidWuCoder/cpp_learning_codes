91d3f2efe48e006b988780b81d61b056ee1aff83
Infinitexxx <468588126@qq.com>
2025-05-28 23:14:45 +0800

    functional

--- test_05_28/test_05_28/main.cpp
+++ test_05_28/test_05_28/main.cpp
@@ -68,20 +68,78 @@ public:
 private:
 	double _rate;
 };
+//int main()
+//{
+//	double rate = 0.49;
+//	// lambda
+//	auto r2 = [rate](double money, int year) {
+//		return money * rate * year;
+//		};
+//	// 函数对象
+//	Rate r1(rate);
+//	r1(10000, 2);
+//	r2(10000, 2);
+//	auto func1 = [] {
+//		cout << "hello world" << endl;
+//		};
+//	func1();
+//	return 0;
+//}
+
+#include<functional>
+int f(int a, int b)
+{
+	return a + b;
+}
+struct Functor
+{
+public:
+	int operator() (int a, int b)
+	{
+		return a + b;
+	}
+};
+class Plus
+{
+public:
+	Plus(int n = 10)
+		:_n(n)
+	{
+	}
+	static int plusi(int a, int b)
+	{
+		return a + b;
+	}
+	double plusd(double a, double b)
+	{
+		return (a + b) * _n;
+	}
+private:
+	int _n;
+};
 int main()
 {
-	double rate = 0.49;
-	// lambda
-	auto r2 = [rate](double money, int year) {
-		return money * rate * year;
-		};
-	// 函数对象
-	Rate r1(rate);
-	r1(10000, 2);
-	r2(10000, 2);
-	auto func1 = [] {
-		cout << "hello world" << endl;
-		};
-	func1();
+	// 包装各种可调⽤对象
+	function<int(int, int)> f1 = f;
+	function<int(int, int)> f2 = Functor();
+	function<int(int, int)> f3 = [](int a, int b) {return a + b; };
+	cout << f1(1, 1) << endl;
+	cout << f2(1, 1) << endl;
+	cout << f3(1, 1) << endl;
+	// 包装静态成员函数
+	// 成员函数要指定类域并且前⾯加&才能获取地址
+	function<int(int, int)> f4 = &Plus::plusi;
+	cout << f4(1, 1) << endl;
+	// 包装普通成员函数
+	// 普通成员函数还有⼀个隐含的this指针参数，所以绑定时传对象或者对象的指针过去都可以
+	function<double(Plus*, double, double)> f5 = &Plus::plusd;
+	Plus pd;
+	cout << f5(&pd, 1.1, 1.1) << endl;
+	function<double(Plus, double, double)> f6 = &Plus::plusd;
+	cout << f6(pd, 1.1, 1.1) << endl;
+	cout << f6(pd, 1.1, 1.1) << endl;
+	function<double(Plus&&, double, double)> f7 = &Plus::plusd;
+	cout << f7(move(pd), 1.1, 1.1) << endl;
+	cout << f7(Plus(), 1.1, 1.1) << endl;
 	return 0;
 }
